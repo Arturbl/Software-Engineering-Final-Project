de5cb8eec41a2bbbffa8c5d7713319b85ccc96c1
Tomás Dias <tomas.dias@appx.pt>
2023-07-07 19:15:33 +0100

    alarm

--- Api/routes/_alarm.js
+++ Api/routes/_alarm.js
@@ -3,30 +3,36 @@ const router = express.Router();
 const routeWrapper = require("../misc/route_wrapper");
 const {body} = require("express-validator");
 const {sha512} = require("sha512-crypt-ts");
+const validateBearerToken = require("../misc/validate_bearer");
 
-router.get('/', routeWrapper(async (req) => {
-    let teste = null
-    if (req.query.userID)
-        return (await req.db.query(`select *
-                                    from "arduino-security-system-postgres-db".registro_alarme
-                                             inner join "arduino-security-system-postgres-db"."User"
-                                                        on "arduino-security-system-postgres-db"."User".userid =
-                                                           "arduino-security-system-postgres-db".registro_alarme.userid`)).rows;
-    await req.db.query(`select *
-                        from "arduino-security-system-postgres-db".registro_alarme`)
-    return teste.rows;
-}));
+router.get('/', validateBearerToken
+    , routeWrapper(async (req) => {
+        let teste = null
+        if (req.query.userID)
+            return (await req.db.query(`select *
+                                        from "arduino-security-system-postgres-db".registro_alarme
+                                                 inner join "arduino-security-system-postgres-db"."User"
+                                                            on "arduino-security-system-postgres-db"."User".userid =
+                                                               "arduino-security-system-postgres-db".registro_alarme.userid`)).rows;
+        await req.db.query(`select *
+                            from "arduino-security-system-postgres-db".registro_alarme`)
+        return teste.rows;
+    }));
 
 router.post('/add',
+    validateBearerToken,
     routeWrapper(async (req) => {
-        await req.db.query(`INSERT INTO "arduino-security-system-postgres-db".registro_alarme(distancia, userid)
+        await req.db.query(`INSERT INTO "arduino-security-system-postgres-db".registro_alarme(distancia,
+                                                                                              register_data,
+                                                                                              turn_off_data)
                             VALUES ($1, $2)`, [req.body.distancia.toFixed(3),
-            req.body.userID]);
+            req.body.register_data, req.body.turn_off_data]);
         return "Create Alarm";
     }));
 
 router.post('/:alarmID/turnOff',
     body("userID").trim().matches(/^\d+$/),
+    validateBearerToken,
     routeWrapper(async (req) => {
         let existentAlarm = await req.db.query(`select *
                                                 from "existentAlarm"
@@ -36,6 +42,18 @@ router.post('/:alarmID/turnOff',
         await req.db.query(`update "existentAlarm"
                             SET userID = req.body.userID
                             where registroid = '${req.params.alarmID}'`);
+        return "Delete Alarm";
+    }));
+
+
+router.post('/turnOn',
+    body("userID").trim().matches(/^\d+$/),
+    validateBearerToken,
+    routeWrapper(async (req) => {
+
+        await req.db.query(`INSERT INTO "arduino-security-system-postgres-db".alarm_status(active, userid)
+                            VALUES ($1, $2)`, [req.body.active,
+            req.body.userID]);
         return "Delete Alarm";
     }));
 
